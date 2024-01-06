f074e0c19048dd8a9f294ca9b3475e81b663ee65
aAAXdbl3 <anilakinali3@gmail.com>
2024-01-06 21:09:25 +0300

    little bit

--- script.js
+++ script.js
@@ -1,157 +1,157 @@
 
-const container = document.querySelector(".container");
-const search = document.querySelector(".search-box button");
-const searchInput = document.querySelector(".search-box input");
-const weatherBox = document.querySelector(".weather-box");
-const weatherDetails = document.querySelector(".weather-details");
-const error404 = document.querySelector(".not-found");
-const cityHide = document.querySelector(".city-hide");
-let lastClickTime = 0;
+  const container = document.querySelector(".container");
+  const search = document.querySelector(".search-box button");
+  const searchInput = document.querySelector(".search-box input");
+  const weatherBox = document.querySelector(".weather-box");
+  const weatherDetails = document.querySelector(".weather-details");
+  const error404 = document.querySelector(".not-found");
+  const cityHide = document.querySelector(".city-hide");
+  let lastClickTime = 0;
 
 
-function searchWeather() {
-  const apiKEY = "863def2fb0e26951383e06cce6199aa3";
-  const city = document.querySelector(".search-box input").value;
+  function searchWeather() {
+    const apiKEY = "863def2fb0e26951383e06cce6199aa3";
+    const city = document.querySelector(".search-box input").value;
 
-  if (city == "") {
-    return;
-  }
-  const now = Date.now();
-  const elapsedTime = now - lastClickTime;
+    if (city == "") {
+      return;
+    }
+    const now = Date.now();
+    const elapsedTime = now - lastClickTime;
 
-  if (elapsedTime < 1500) {
-    // Eğer son tıklamadan bu yana 2 saniyeden kısa bir süre geçtiyse işlem yapma
-    return;
-  }
+    if (elapsedTime < 1500) {
+      // Eğer son tıklamadan bu yana 2 saniyeden kısa bir süre geçtiyse işlem yapma
+      return;
+    }
 
-  lastClickTime = now;
+    lastClickTime = now;
 
-  fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEY}`
-  )
-    .then((response) => response.json())
-    .then((json) => {
-      if (json.cod == "404") {
-        cityHide.textContent = city;
-        container.style.height = "400px";
-        weatherBox.classList.remove("active");
-        weatherDetails.classList.remove("active");
-        error404.classList.add("active");
-        return;
-      }
-
-      console.log("Gelen Veri:", json); // Gelen veriyi konsola yazdır
-      const image = document.querySelector(".weather-box img");
-      const temperature = document.querySelector(".weather-box .temperature");
-      const description = document.querySelector(".weather-box .description");
-      const humidity = document.querySelector(
-        ".weather-details .humidity span"
-      );
-      const wind = document.querySelector(".weather-details .wind span");
-
-      if (cityHide.textContent == city) {
-        return;
-      } else {
-        cityHide.textContent = city;
-        container.style.height = "555px";
-        weatherBox.classList.add("active");
-        container.classList.add("active");
-        weatherDetails.classList.add("active");
-        error404.classList.remove("active");
-
-        setTimeout(() => {
-          container.classList.remove("active");
-        }, 1500);
-
-        console.log(json.weather[0].main);
-        switch (json.weather[0].main) {
-          case "Clear":
-            image.src = "src/images/clear.png";
-            break;
-          case "Rain":
-            image.src = "src/images/rain.png";
-            break;
-          case "Snow":
-            image.src = "src/images/snow.png";
-            break;
-          case "Clouds":
-            image.src = "src/images/clouds.png";
-            break;
-          case "Mist":
-          case "Haze":
-            image.src = "src/images/mist.png";
-            break;
-          default:
-            image.src = "src/images/clear.png";
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKEY}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.cod == "404") {
+          cityHide.textContent = city;
+          container.style.height = "400px";
+          weatherBox.classList.remove("active");
+          weatherDetails.classList.remove("active");
+          error404.classList.add("active");
+          return;
         }
 
-        temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
-        description.innerHTML = `${json.weather[0].description}`;
-        humidity.innerHTML = `${json.main.humidity}<span>%</span>`;
-        wind.innerHTML = `${parseInt(json.wind.speed)}<span>Km/h</span>`;
-
-        const infoWeather = document.querySelector(".weather-info");
-        const infoHumidity = document.querySelector(".humidity-info");
-        const infoWind = document.querySelector(".wind-info");
-
-        const elCloneInfoWeather = infoWeather.cloneNode(true);
-        const elCloneInfoHumidity = infoHumidity.cloneNode(true);
-        const elCloneInfoWind = infoWind.cloneNode(true);
-
-        elCloneInfoWeather.id = "clone-info-weather";
-        elCloneInfoWeather.classList.add("active-clone");
-
-        elCloneInfoHumidity.id = "clone-info-humidity";
-        elCloneInfoHumidity.classList.add("active-clone");
-
-        elCloneInfoWind.id = "clone-info-wind";
-        elCloneInfoWind.classList.add("active-clone");
-
-        setTimeout(() => {
-          infoWeather.insertAdjacentElement("afterend", elCloneInfoWeather);
-          infoHumidity.insertAdjacentElement("afterend", elCloneInfoHumidity);
-          infoWind.insertAdjacentElement("afterend", elCloneInfoWind);
-        }, 1000);
-
-        const cloneInfoWeather = document.querySelectorAll(
-          ".weather-info.active-clone"
+        console.log("Gelen Veri:", json); // Gelen veriyi konsola yazdır
+        const image = document.querySelector(".weather-box img");
+        const temperature = document.querySelector(".weather-box .temperature");
+        const description = document.querySelector(".weather-box .description");
+        const humidity = document.querySelector(
+          ".weather-details .humidity span"
         );
-        const totalCloneInfoWeather = cloneInfoWeather.length;
-        const cloneInfoWeatherFirst = cloneInfoWeather[0];
+        const wind = document.querySelector(".weather-details .wind span");
 
-        const cloneInfoHumidity = document.querySelectorAll(
-          ".humidity-info.active-clone"
-        );
-        const cloneInfoHumidityFirst = cloneInfoHumidity[0];
-
-        const cloneInfoWind = document.querySelectorAll(
-          ".wind-info.active-clone"
-        );
-        const cloneInfoWindFirst = cloneInfoWind[0];
-
-        if (totalCloneInfoWeather > 0) {
-          cloneInfoWeatherFirst.classList.remove('active-clone');
-          cloneInfoHumidityFirst.classList.remove('active-clone');
-          cloneInfoWindFirst.classList.remove('active-clone');
+        if (cityHide.textContent == city) {
+          return;
+        } else {
+          cityHide.textContent = city;
+          container.style.height = "555px";
+          weatherBox.classList.add("active");
+          container.classList.add("active");
+          weatherDetails.classList.add("active");
+          error404.classList.remove("active");
 
           setTimeout(() => {
-            cloneInfoWeatherFirst.remove();
-            cloneInfoHumidityFirst.remove();
-            cloneInfoWindFirst.remove();
+            container.classList.remove("active");
+          }, 1500);
+
+          console.log(json.weather[0].main);
+          switch (json.weather[0].main) {
+            case "Clear":
+              image.src = "src/images/clear.png";
+              break;
+            case "Rain":
+              image.src = "src/images/rain.png";
+              break;
+            case "Snow":
+              image.src = "src/images/snow.png";
+              break;
+            case "Clouds":
+              image.src = "src/images/clouds.png";
+              break;
+            case "Mist":
+            case "Haze":
+              image.src = "src/images/mist.png";
+              break;
+            default:
+              image.src = "src/images/clear.png";
+          }
+
+          temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+          description.innerHTML = `${json.weather[0].description}`;
+          humidity.innerHTML = `${json.main.humidity}<span>%</span>`;
+          wind.innerHTML = `${parseInt(json.wind.speed)}<span>Km/h</span>`;
+
+          const infoWeather = document.querySelector(".weather-info");
+          const infoHumidity = document.querySelector(".humidity-info");
+          const infoWind = document.querySelector(".wind-info");
+
+          const elCloneInfoWeather = infoWeather.cloneNode(true);
+          const elCloneInfoHumidity = infoHumidity.cloneNode(true);
+          const elCloneInfoWind = infoWind.cloneNode(true);
+
+          elCloneInfoWeather.id = "clone-info-weather";
+          elCloneInfoWeather.classList.add("active-clone");
+
+          elCloneInfoHumidity.id = "clone-info-humidity";
+          elCloneInfoHumidity.classList.add("active-clone");
+
+          elCloneInfoWind.id = "clone-info-wind";
+          elCloneInfoWind.classList.add("active-clone");
+
+          setTimeout(() => {
+            infoWeather.insertAdjacentElement("afterend", elCloneInfoWeather);
+            infoHumidity.insertAdjacentElement("afterend", elCloneInfoHumidity);
+            infoWind.insertAdjacentElement("afterend", elCloneInfoWind);
           }, 1000);
+
+          const cloneInfoWeather = document.querySelectorAll(
+            ".weather-info.active-clone"
+          );
+          const totalCloneInfoWeather = cloneInfoWeather.length;
+          const cloneInfoWeatherFirst = cloneInfoWeather[0];
+
+          const cloneInfoHumidity = document.querySelectorAll(
+            ".humidity-info.active-clone"
+          );
+          const cloneInfoHumidityFirst = cloneInfoHumidity[0];
+
+          const cloneInfoWind = document.querySelectorAll(
+            ".wind-info.active-clone"
+          );
+          const cloneInfoWindFirst = cloneInfoWind[0];
+
+          if (totalCloneInfoWeather > 0) {
+            cloneInfoWeatherFirst.classList.remove('active-clone');
+            cloneInfoHumidityFirst.classList.remove('active-clone');
+            cloneInfoWindFirst.classList.remove('active-clone');
+
+            setTimeout(() => {
+              cloneInfoWeatherFirst.remove();
+              cloneInfoHumidityFirst.remove();
+              cloneInfoWindFirst.remove();
+            }, 1000);
+          }
+
         }
+      })
+      .catch((error) => {
+        console.error("Hata Oluştu:", error); // Hata durumunu konsola yazdır
+      });
+  };
+  search.addEventListener("click", searchWeather);
 
-      }
-    })
-    .catch((error) => {
-      console.error("Hata Oluştu:", error); // Hata durumunu konsola yazdır
-    });
-};
-search.addEventListener("click", searchWeather);
-
-searchInput.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    searchWeather();
-  }
-});
+  searchInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      searchWeather();
+    }
+  });
 
---
+++ time.js
@@ -0,0 +1,32 @@
+// Dijital Saat
+function startTime() {
+  var today = new Date();
+  var hr = today.getHours();
+  var min = today.getMinutes();
+  var sec = today.getSeconds();
+  ap = (hr < 12) ? "AM" : "PM";
+  hr = (hr == 0) ? 12 : hr;
+  hr = (hr > 12) ? hr - 12 : hr;
+  hr = checkTime(hr);
+  min = checkTime(min);
+  sec = checkTime(sec);
+  document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+  
+  var months = ['Ocak', 'Şubat', 'Mart', 'Nisan', 'Mayıs', 'Haziran', 'Temmuz', 'Ağustos', 'Eylul', 'Ekim', 'Kasım', 'Aralık'];
+  var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  var curWeekDay = days[today.getDay()];
+  var curDay = today.getDate();
+  var curMonth = months[today.getMonth()];
+  var curYear = today.getFullYear();
+  var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+  document.getElementById("date").innerHTML = date;
+  
+  var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+  if (i < 10) {
+      i = "0" + i;
+  }
+  return i;
+}
+startTime();
